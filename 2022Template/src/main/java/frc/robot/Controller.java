a64d8fed77191f0c072b2aebdd0253f60f41cae5
Kyle <45471789+kphillips2003@users.noreply.github.com>
2022-01-15 12:38:42 -0500

    Added 2022 Template to Connor H's branch - Kyle
    Issues I had to delete his branch to do this

---
+++ 2022Template/src/main/java/frc/robot/Controller.java
@@ -0,0 +1,20 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+//Example Code (Make what you need) BUT DONT CREATE YOUR OWN CONTROLLER CLASS
+
+public class Controller {
+    // public Joystick transStick;
+    // public JoystickButton transStick_Button1;
+
+    public Controller() {
+        // transStick = new Joystick(1);
+        // transStick_Button1 = new JoystickButton(transStick, 1);
+    }
+
+    // public JoystickButton getTransStick_Button1() {
+    //     // return transStick_Button1;
+    // }
+}
